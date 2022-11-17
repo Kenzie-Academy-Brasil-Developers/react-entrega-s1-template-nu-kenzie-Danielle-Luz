9cdc81a5b6a200281e2c87fc21cf099e0bf8d492
Axionauta <daniln2209@gmail.com>
2022-11-17 00:10:21 -0300

    feat: create component structure

--- nukenzie/src/components/Card/index.jsx
+++ nukenzie/src/components/Card/index.jsx
@@ -1,1 +1,19 @@
 import "./styles.css";
+import { ButtonIcon } from "../Button";
+
+export default function Card({ title, category, price }) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <div>
+          <span>{price}</span>
+          <ButtonIcon />
+        </div>
+      </div>
+      <span>
+        {category}
+      </span>
+    </article>
+  );
+}
