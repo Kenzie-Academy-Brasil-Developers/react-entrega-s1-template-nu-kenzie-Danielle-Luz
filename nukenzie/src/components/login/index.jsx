149bbe8590eb0ed0609ca05f01da259412c20ba3
Axionauta <daniln2209@gmail.com>
2022-11-17 18:15:25 -0300

    feat: add login text

--- nukenzie/src/components/login/index.jsx
+++ nukenzie/src/components/login/index.jsx
@@ -1,2 +1,16 @@
 import "./styles.css";
 
+export default function Login() {
+  return (
+    <div className="wrapper">
+      <main className="main-content">
+        <h1 className="logo">
+          <span className="pink">Nu</span> Kenzie
+        </h1>
+        <h2 className="title-1">Centralize o controle das suas finanças</h2>
+        <p className="text-1">de forma rápida e segura</p>
+        <button className="button button-primary">Iniciar</button>
+      </main>
+    </div>
+  );
+}
--- nukenzie/src/index.js
+++ nukenzie/src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<Home />);
+root.render(<Login />);
