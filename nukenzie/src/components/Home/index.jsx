a25131eadafbf37575f1c2ec6b132bcc8f6b812d
Axionauta <daniln2209@gmail.com>
2022-11-17 03:26:38 -0300

    feat: add second section components

--- nukenzie/src/components/Home/index.jsx
+++ nukenzie/src/components/Home/index.jsx
@@ -5,10 +5,12 @@ import Total from "../Total";
 import Form from "../Form";
 import { ButtonSecondary } from "../Button";
 import { Card, EmptyCard } from "../Card";
+
 import { useState } from "react";
 
 export default function Home() {
-  const [register, setRegister] = useState([]);
+  const categorys = ["Todos", "Entradas", "Despesas"];
+  const [registers, setRegisters] = useState([]);
 
   return (
     <div className="page-wrapper">
@@ -18,7 +20,40 @@ export default function Home() {
           <Form />
           <Total />
         </section>
-        <section></section>
+        <section className="financial-resume-wrapper">
+          <article className="financial-resume">
+            <h3 className="financial-title">Resumo financeiro</h3>
+            <div className="button-group">
+              {categorys.map((category) => {
+                return <ButtonSecondary text={category} />;
+              })}
+            </div>
+          </article>
+          {registers.length == 0 && (
+            <h2 className="empty-message">
+              Você ainda não possui nenhum lançamento
+            </h2>
+          )}
+          <section className="cards-container">
+            {registers.length == 0 ? (
+              <>
+                <EmptyCard />
+                <EmptyCard />
+                <EmptyCard />
+              </>
+            ) : (
+              registers.map((register) => {
+                return (
+                  <Card
+                    title={register.title}
+                    category={register.category}
+                    price={register.price}
+                  />
+                );
+              })
+            )}
+          </section>
+        </section>
       </main>
     </div>
   );
