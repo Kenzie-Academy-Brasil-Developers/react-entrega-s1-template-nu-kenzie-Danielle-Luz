8ead12345516becf7c86be6e92986b1b636dcf1e
Axionauta <daniln2209@gmail.com>
2022-11-17 02:30:39 -0300

    feat: create and style form component

--- nukenzie/src/components/Form/index.jsx
+++ nukenzie/src/components/Form/index.jsx
@@ -1,8 +1,21 @@
 import "./styles.css";
 import { InputText, InputValue } from "../Input";
 import { ButtonPrimary } from "../Button";
-import Total from "../Total";
+import { Select } from "../Select";
 
-export default function Form () {
-
+export default function Form() {
+  return (
+    <form className="form">
+      <InputText
+        placeholder="Digite aqui sua descrição"
+        label="Descrição"
+        info="Ex: Compra de roupas"
+      />
+      <div className="input-group">
+        <InputValue onChange={() => {}} label="Valor" />
+        <Select options={["Entrada", "Despesa"]} label="Tipo de valor" />
+      </div>
+      <ButtonPrimary text="Inserir valor" />
+    </form>
+  );
 }
