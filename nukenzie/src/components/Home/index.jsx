09aae0f888816fdb8830ca5dd40e26855fea438e
Axionauta <daniln2209@gmail.com>
2022-11-17 02:53:38 -0300

    feat: add first section code

---
+++ nukenzie/src/components/Home/index.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+import Header from "../Header";
+import Total from "../Total";
+import Form from "../Form";
+import { ButtonSecondary } from "../Button";
+import Card from "../Card";
+import { useState } from "react";
+
+export default function Home () {
+  const [register, setRegister] = useState([]);
+
+  return (
+    <div className="page-wrapper">
+      <Header />
+      <main className="main-content">
+        <section className="payment-info">
+          <Form />
+          <Total />
+        </section>
+      </main>
+    </div>
+  );
+}
