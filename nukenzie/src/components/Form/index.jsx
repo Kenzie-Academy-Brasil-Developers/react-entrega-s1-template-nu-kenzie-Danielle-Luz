dca5000cde0a56da84ae4a4afdc6069829e10006
Axionauta <daniln2209@gmail.com>
2022-11-17 01:28:18 -0300

    feat: import components

--- nukenzie/src/components/Form/index.jsx
+++ nukenzie/src/components/Form/index.jsx
@@ -1,1 +1,8 @@
 import "./styles.css";
+import { InputText, InputValue } from "../Input";
+import { ButtonPrimary } from "../Button";
+import Total from "../Total";
+
+export default function Form () {
+
+}
