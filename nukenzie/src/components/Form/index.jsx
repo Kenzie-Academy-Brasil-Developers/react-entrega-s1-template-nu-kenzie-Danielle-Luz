ae6dafc90e60e439f3352ce556e4895078db0de1
Axionauta <daniln2209@gmail.com>
2022-11-17 10:45:37 -0300

    feat: create function to get form data

--- nukenzie/src/components/Form/index.jsx
+++ nukenzie/src/components/Form/index.jsx
@@ -3,37 +3,49 @@ import { InputText, InputValue } from "../Input";
 import { ButtonPrimary } from "../Button";
 import { Select } from "../Select";
 
-export default function Form({registers, setRegisters}) {
+function getFormData(form) {
+  let data = {};
+  const formElements = form.querySelectorAll("input, [data-selected]");
+
+  formElements.forEach((formElement) => {
+    if (formElement.tagName == "INPUT") {
+      data = {
+        ...data,
+        [formElement.name]: formElement.value,
+      };
+    } else {
+      data = {
+        ...data,
+        category: formElement.getAttribute("data-selected"),
+      };
+    }
+  });
+
+  return data;
+}
+
+export default function Form({ registers, setRegisters }) {
   return (
-    <form className="form" onSubmit={event => {
-      event.preventDefault();
+    <form
+      className="form"
+      onSubmit={(event) => {
+        event.preventDefault();
 
-      let data = {};
-      const form = event.target;
-      const formElements = form.querySelectorAll("input, [data-selected]");
+        const form = event.target;
 
-      formElements.forEach((formElement) => {
-        if (formElement.tagName == "INPUT") {
-          data = {
-            ...data,
-            [formElement.name]: formElement.value
-          }
+        const data = getFormData(form);
+
+        const dataIsNotEmpty = Object.values(data).every(
+          (value) => value != ""
+        );
+
+        if (dataIsNotEmpty) {
+          setRegisters([...registers, data]);
         } else {
-          data = {
-            ...data,
-            "category": formElement.getAttribute("data-selected")
-          }
+          // toastfy
         }
-      })
-
-      const dataIsNotEmpty = Object.values(data).every((value) => value != "");
-
-      if (dataIsNotEmpty) {
-        setRegisters([...registers, data])
-      } else {
-        // toastfy
-      }
-    }}>
+      }}
+    >
       <InputText
         name="title"
         placeholder="Digite aqui sua descrição"
