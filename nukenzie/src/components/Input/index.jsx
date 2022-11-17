13cd2d2937c7bba35938d062c0400d011b7cb005
Axionauta <daniln2209@gmail.com>
2022-11-16 22:11:16 -0300

    feat: add input value component

--- nukenzie/src/components/Input/index.jsx
+++ nukenzie/src/components/Input/index.jsx
@@ -5,3 +5,12 @@ export function InputText ({placeholder}) {
     <input className="input input-text" type="text" placeholder={placeholder} />
   );
 }
+
+export function InputValue ({onChange}) {
+  return (
+    <div className="input-wrapper">
+      <input className="input input-text" type="number" placeholder="1" onChange={onChange} />
+      <span className="price-sign">R$</span>
+    </div>
+  );
+}
