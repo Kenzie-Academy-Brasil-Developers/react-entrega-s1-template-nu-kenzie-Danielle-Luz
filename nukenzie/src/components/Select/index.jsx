b29525085fad4eeab4a4b3b9a256e5ae51598275
Axionauta <daniln2209@gmail.com>
2022-11-16 23:46:51 -0300

    feat: create and style header component

--- nukenzie/src/components/Select/index.jsx
+++ nukenzie/src/components/Select/index.jsx
@@ -1,1 +1,15 @@
 import "./styles.css";
+import { ButtonSecondary } from "./../Button";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="logo">
+          <span className="pink">Nu</span> Kenzie
+        </h1>
+        <ButtonSecondary text="Início" />
+      </div>
+    </header>
+  );
+}
